abfb0f6594bf8c35d99bf0e91c415a5739524d77
qingsheng luo <129710265+qinghe-oasis@users.noreply.github.com>
2023-05-28 10:26:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ available_setting = {
     # openai api配置
     "open_ai_api_key": "",  # openai api key
     # openai apibase，当use_azure_chatgpt为true时，需要设置对应的api base
-    "open_ai_api_base": "https://api.openai.com/v1",
+    "open_ai_api_base": "https://openai.al123.cn/v1",
     "proxy": "",  # openai使用的代理
     # chatgpt模型， 当use_azure_chatgpt为true时，其名称为Azure上model deployment名称
     "model": "gpt-3.5-turbo",
